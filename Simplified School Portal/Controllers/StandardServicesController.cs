61afbdd3e2e240ce0b0f02b1c25e5240d7d6a41b
Tobias Sagis <tobiassagis@gmail.com>
2018-03-27 23:15:15 +0200

    first changes to layout and bootstrap, controllers.

---
+++ Simplified School Portal/Controllers/StandardServicesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simplified_School_Portal.Controllers
+{
+    public class StandardServicesController : Controller
+    {
+        // GET: StandardServices
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Studentenplein()
+        {
+            return View();
+        }
+
+        public ActionResult Canvas()
+        {
+            return View();
+        }
+
+        public ActionResult Lesrooster()
+        {
+            return View();
+        }
+
+        public ActionResult ServiceRequest()
+        {
+            return View();
+        }
+    }
+}
